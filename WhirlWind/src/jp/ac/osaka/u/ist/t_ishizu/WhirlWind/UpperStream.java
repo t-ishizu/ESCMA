1afbd952f25ede8598f133c1e6c86aef1c28baf5
takuya ishizu <t-ishizu@ist.osaka-u.ac.jp>
2016-12-16 16:43:03 +0900

    Tokenクラスの追加

--- WhirlWind/src/jp/ac/osaka/u/ist/t_ishizu/WhirlWind/UpperStream.java
+++ WhirlWind/src/jp/ac/osaka/u/ist/t_ishizu/WhirlWind/UpperStream.java
@@ -6,7 +6,9 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
+import static jp.ac.osaka.u.ist.t_ishizu.WhirlWind.WhirlWind.*;
 /**
  * 
  * @author t-ishizu
@@ -68,6 +70,58 @@ public class UpperStream {
 			System.exit(0);
 		}
 		return fileList;
+	}
+	
+	public static final ArrayList<Sprout> createSproutList(){
+		if(CobolSeedFile==null){
+			System.err.println("!Error:Cobol Source Code isn't defined.");
+			System.exit(0);
+		}
+		BufferedReader br = getBufferedReader(CobolSeedFile);
+		ArrayList<Sprout> sproutList = new ArrayList<Sprout>();
+		try{
+			String str = br.readLine();
+			boolean find = false;
+			Sprout sp = null;
+			while(str!=null){
+				if(str.equals("#begin{set}")){
+					find = true;
+					sp = new Sprout().setId(sproutList.size());
+				}else if(str.equals("#end{set}")){
+					find = false;
+					sproutList.add(sp);
+				}else if(find){
+					/* register seed information */
+					String[] str_split = str.split("[.,\t]+",0);
+					Seed s = new Seed()
+					.setFileId(Integer.parseInt(str_split[1]))
+					.setLS(Integer.parseInt(str_split[2])).setLE(Integer.parseInt(str_split[5]))
+					.setCS(Integer.parseInt(str_split[3])).setCE(Integer.parseInt(str_split[6]))
+					.setTS(Integer.parseInt(str_split[4])).setTE(Integer.parseInt(str_split[7]))
+					.setSprout(sp);
+					sp.addSeedList(s);
+				}
+				str=br.readLine();
+			}
+		}catch(IOException e){
+			System.out.println(e.getMessage());
+			System.exit(0);
+		}
+		return sproutList;
+	}
+	
+	
+	public static final HashMap<Integer,ArrayList<Seed>> createSeedMap(){
+		HashMap<Integer,ArrayList<Seed>> SeedMap = new HashMap<Integer,ArrayList<Seed>>();
+		for(Sprout sp : sproutList){
+			for(Seed s : sp.getSeedList()){
+				if(!SeedMap.containsKey(s.getFileId())){
+					SeedMap.put(s.getFileId(),new ArrayList<Seed>());
+				}
+				SeedMap.get(s.getFileId()).add(s);
+			}
+		}
+		return SeedMap;
 	}
 	private UpperStream(){}
 }
--- WhirlWind/src/jp/ac/osaka/u/ist/t_ishizu/WhirlWind/WhirlWind.java
+++ WhirlWind/src/jp/ac/osaka/u/ist/t_ishizu/WhirlWind/WhirlWind.java
@@ -8,16 +8,22 @@ package jp.ac.osaka.u.ist.t_ishizu.WhirlWind;
 import static jp.ac.osaka.u.ist.t_ishizu.WhirlWind.UpperStream.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 public class WhirlWind {
 	/**
 	 * @author t-ishizu
 	 * 
 	 */
 	public static ArrayList<String> fileList; 
+	public static ArrayList<Sprout> sproutList;
+	public static HashMap<Integer, ArrayList<Seed>> SeedMap;
 	public WhirlWind(){
 		
 	}
 	public void run(){
+		/* create ArrayList by UpperStream class */ 
 		fileList = createFileList();
+		sproutList = createSproutList();
+		SeedMap = createSeedMap();
 	}
 }
